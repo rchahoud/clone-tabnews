f2cdfc8d72b4e3a049f03e36db8bb7238658c72c
rchahoud <97749217+rchahoud@users.noreply.github.com>
2025-11-18 00:48:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Marilinha, se vc é uma pessoa bacana dá um sorrisinho =)</h1>
+    return <h1>Oi Marilinha, se vc é uma pessoa bacana dá um sorrisinho 😜</h1>
 }
 
 export default Home;
